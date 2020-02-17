81d1a327dcee7481230ef02388ec5e676bfd7a16
구민구 <koomk97@naver.com>
2020-02-17 14:52:15 +0900

    GUI start
    GUI base making

--- cjubot_library.py
+++ cjubot_library.py
@@ -4,6 +4,7 @@
 
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+from urllib.parse   import quote # for URL 한글 인코딩
 
 if __name__ == "__main__":
     print("This is main")
@@ -57,7 +58,74 @@ def bookCode(url, num):
         print('잘못된 입력입니다.')
         return 'err'
     
+# 도서 총 리스트 검색 / 무엇으로 검색 / 도서 이름/ 전체출력여부 (전체출력 print_all ==100)
+def bookListSearch(searchBy, name, print_all): 
+    #URL 앞쪽
+    url_listFront = "https://library.cju.ac.kr/search/tot/result?pn="
+    #키워드로 검색
+    keyword_end = "&folder_id=null&q="+quote(name)+"&st=KWRD&si=TOTAL"
+    #전체일치 검색
+    allSame_end = "&folder_id=null&q="+quote(name)+"&st=EXCT&si=TOTAL"
+    # 1페이지부터 시작 
+    pageNumber = 1
+
+    # URL 조합 / 앞쪽 + 페이지번호 + 무엇으로 검색
+    if searchBy == '1':
+        URL_end = keyword_end
+    else:
+        URL_end = allSame_end
     
+    url = url_listFront+ str(pageNumber) + URL_end
+    repeat =1  # 출력 횟수 
+    bookList = bookListCount(url) # 도서 검색 총 개수
+    
+    #전체출력의 경우
+    if print_all == 100:
+        repeat = int(bookList/10)+1
+        if bookList%10 == 0: # 10의 배수일 경우
+            repeat -=1
+    
+    # 검색된 책 목록 출력
+    for i in range(repeat):
+        #URL 갱신
+        url = url_listFront +str(pageNumber) +URL_end
+        pageNumber +=1 #page 번호 추가
+        info = bookListInfo(url) #페이지 당 도서 정보 딕셔너리
+        for p in info: #10개씩 출력
+            print(info[p][0]+'\n'+info[p][2]) #제목 / 출판사
+            print(info[p][1]+"\n") #저자
+    #  총 검색 도서 개수 / URL를 리스트로 반환
+    return [bookList, url_listFront +'1'+URL_end]
+    
+# 책 선택하고 책 상세정보 출력 
+def bookDetailSearch(code):
+    #책 상세정보 URL 앞쪽
+    url_bookDetail_front = "https://library.cju.ac.kr/search/detail/"
+    url = url_bookDetail_front+code #URL 조합
+    #도서 선택에서 인덱스 에러 경우
+    if code =='err':
+        return 0
+    
+    getHTML = urlopen(url)
+    bs = BeautifulSoup(getHTML, "html.parser")
+    
+    bookHead = bs.find("div", {"class": "profileHeader"}) #제목+저자
+    bookTitle = bookHead.find("h3").get_text() #제목
+    bookAuthor = bookHead.find("p").get_text() #저자
+    bookMakerList = bs.findAll("th", {"scope": "row"}) #도서정보 종합 리스트
+    bookMaker = "정보 없음" #출판사 정보 없을 경우
+    for book in bookMakerList:
+        if book.get_text() == "발행사항": #출판사 정보 있을 경우
+            bookMaker = book.next_sibling.get_text().split(",")[0].split(": ")[-1]
+            break
+            
+    bookLocation = bs.find("td", {"class": "callNum"}).get_text() #도서관에서 도서 위치
+    bookRental = bs.find("span", {"class": "status available"}).get_text() #대출가능여부
+    
+    bookDetail = [bookTitle, bookAuthor, \
+                bookMaker, bookLocation, bookRental]
+    print(bookDetail)
+    return bookDetail
     
     
     
