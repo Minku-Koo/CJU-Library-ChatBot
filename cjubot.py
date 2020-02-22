4c4ff9efc6fe9c54bcb6983f4635bee62e939dd6
구민구 <koomk97@naver.com>
2020-02-22 19:31:53 +0900

    Update cjubot.py

--- cjubot.py
+++ cjubot.py
@@ -8,9 +8,9 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import sys
 from PyQt5.QtWidgets import *
-from PyQt5.QtGui import QIcon, QFont
+from PyQt5.QtGui import QIcon, QFont,QPalette
 from cjubot_library import *
-from PyQt5.QtCore import QDate, Qt # QT 날짜 모듈
+from PyQt5.QtCore import QDate, Qt ,QSize# QT 날짜 모듈
 
 
 #도서 검색 메인 / 최초 검색 시도 시 name == '' 
@@ -57,6 +57,8 @@ class MyApp(QWidget):
 
     def __init__(self):
         super().__init__()
+        self.centerHeight = 650
+        self.widgetHeight = 0
         self.initUI()
 
     def initUI(self):
@@ -83,18 +85,23 @@ class MyApp(QWidget):
         lb_head.setFont(QFont('굴림',17, QFont.Bold))
         
         self.lb_center = QLabel()
-        self.lb_center.setMaximumSize(550,660) #라벨 최대사이즈
+        #self.lb_center.setFixedSize(550,650)
+        
+        self.lb_center.setFixedHeight(self.centerHeight)
+        #self.lb_center.setMinimumSize(QSize(550, 500))
+        #self.lb_center.setMaximumSize(550,660) #라벨 최대사이즈
         self.lb_center.setAlignment(Qt.AlignCenter)
         self.lb_center.setStyleSheet("color : black;" #라벨 꾸미기
                               #"border-style: solid;"
-                              #"border-width: 2px;"
+                              #"border-width: 3px;"
                               #"border-color: black;"
                               #"border-radius: 2px;"
-                              #"background-color: white;"
+                              #"background-color: orange;"
                             )
         
         
         self.lb_bottom = QLabel()
+        
         #lb_bottom.setAlignment(Qt.AlignBottom)
         self.lb_bottom.setMaximumSize(55000,80) #라벨 최대사이즈
         self.lb_bottom.setStyleSheet("color : black;" #라벨 꾸미기
@@ -110,17 +117,27 @@ class MyApp(QWidget):
         #chatBox.addWidget(lb_head)
         #chatBox.addStretch(1) #빈공간 설정
         #lb_center 라벨에 스크롤 설정
-        scroll = QScrollArea()
-        scroll.setWidget(self.lb_center)
-        scroll.setFixedHeight(660)
-        scroll.setWidgetResizable(True)
+        self.scroll = QScrollArea()
+        #QMainWindow().setCentralWidget(self.scroll)
+        #self.scroll.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn) # vertical 스크롤바 항상 on
+        self.scroll.setVerticalScrollBarPolicy(Qt.ScrollBarAsNeeded) # vertical 스크롤바 필요시 on
+        #self.setCentralWidget(self.scroll)
+        self.scroll.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff) # horizon 스크롤 항상 off
+        self.scroll.setWidget(self.lb_center) # scroll area에 라벨 추가 
+        self.scroll.setFixedHeight(650) # 스크롤창 고정 세로 크기 
+        self.scroll.setWidgetResizable(True)
+        #self.setCentralWidget(self.scroll)
+        
+        
         
         #레이아웃에 라벨 추가
         self.mainLayout = QVBoxLayout()
         self.mainLayout.addWidget(lb_head)
-        self.mainLayout.addWidget(scroll) #라벨 말고 qscrollarea를 입력해야함
+        self.mainLayout.addWidget(self.scroll) #라벨 말고 qscrollarea를 입력해야함
         self.mainLayout.addWidget(self.lb_bottom)
         self.mainLayout.setAlignment(Qt.AlignTop)
+        
+        
         
         print('-=-=')
         self.setLayout(self.mainLayout) #main layout
@@ -129,6 +146,7 @@ class MyApp(QWidget):
         
         self.show() #화면에 표시
         
+    
     def center(self): #어플을 화면 중심에 위치시키는 함수
         qr = self.frameGeometry() #창의 위치 크기 정보 불러옴
         cp = QDesktopWidget().availableGeometry().center() #화면 가운데 위치 식별
@@ -137,6 +155,10 @@ class MyApp(QWidget):
         
     def btn1_click(self): #btn1 클릭이벤트
         print("button click")
+    
+    #  #  #  #  #  #  #  #  #  #  #  #
+    # 이용시간 클릭 이벤트 버튼 #
+    #   #  #  #  #  #  #   #  #  #  #
     
     
     def referenceRoom_click(self): 
@@ -176,6 +198,10 @@ class MyApp(QWidget):
         #이용시간 메뉴 텍스트 설정
         useTimeText1 = QLabel('이용을 원하시는 시설을 클릭해주세요.')
         useTimeText1.setMaximumSize(330,30)
+        
+        
+        #useTimeText1.setFixedHeight(30)
+        #useTimeText1.setFixedSize(300, 30)
         useTimeText1.setFont(QFont('굴림',11))
         useTimeText1.setStyleSheet("color : black;" #라벨 꾸미기
                               "border-style: solid;"
@@ -221,6 +247,11 @@ class MyApp(QWidget):
         useTimeLayout.addWidget(self.copyPrint, 1, 2)
         useTimeLayout.addWidget(self.start_btn, 1, 3)
         
+        self.widgetHeight+=90
+        if self.widgetHeight >= 650: #스크롤바 늘려주기
+            self.centerHeight+=90
+            self.lb_center.setFixedHeight(self.centerHeight)
+        
         useTimeLayout.setAlignment(Qt.AlignTop)
         #centerlayout에 이용시간 버튼 집합 레이아웃 추가
         centerLayout.addLayout(useTimeLayout)
@@ -236,6 +267,9 @@ class MyApp(QWidget):
         txt2 = '학생증 또는 모바일 이용증이 있어야 출입 할 수 있으니\n도서관에 올 때는 학생증을 지참하여야 합니다.'
         findWayText1 = QLabel(txt1 + txt2) # 텍스트 합쳐서 입력
         findWayText1.setMaximumSize(450,100)
+        
+        #findWayText1.setFixedHeight(100)
+        #findWayText1.setFixedSize(450, 100)
         findWayText1.setFont(QFont('굴림',10))
         findWayText1.setStyleSheet("color : black;" #라벨 꾸미기
                               "border-style: solid;"
@@ -246,6 +280,8 @@ class MyApp(QWidget):
                             )
         findWayText2 = QLabel('<출입방법>\n입구통제기의 스캐너에 학생증(모바일 이용증) 스캐닝 → 녹색램프 → 입장')
         findWayText2.setMaximumSize(520,60)
+        #findWayText2.setFixedHeight(60)
+        #findWayText2.setFixedSize(520, 60)
         findWayText2.setFont(QFont('굴림',9))
         findWayText2.setStyleSheet("color : black;" #라벨 꾸미기
                               "border-style: solid;"
@@ -257,7 +293,10 @@ class MyApp(QWidget):
                             
         centerLayout.addWidget(findWayText1) #레이아웃에 텍스트 입력
         centerLayout.addWidget(findWayText2)
-        
+        self.widgetHeight+=150
+        if self.widgetHeight >= 650: #스크롤바 늘려주기
+            self.centerHeight+=150
+            self.lb_center.setFixedHeight(self.centerHeight)
         centerLayout.setAlignment(Qt.AlignTop)
         #centerlayout에 출입안내 버튼 집합 레이아웃 추가
         self.startMessage() #최초 출력 함수 
@@ -267,78 +306,111 @@ class MyApp(QWidget):
         
     def infra_click(self): #시설 안내
         print("infra_click button click")
+        #시설 안내 메뉴 텍스트 설정
+        txt = '이용을 원하시는 시설을 클릭하세요.'
+        infraText = QLabel(txt)
+        infraText.setMaximumSize(450,30)
+        infraText.setFont(QFont('굴림',10))
+        infraText.setStyleSheet("color : black;" #라벨 꾸미기
+                              "border-style: solid;"
+                              "border-width: 1px;"
+                              "border-color: black;"
+                              "border-radius: 2px;"
+                              "background-color: #CEF6E3;"
+                            )
+        #버튼 집합
+        infraMenuLayout = QGridLayout() # 버튼 집합 레이아웃 설정
+        
+        self.monileApp = QPushButton("모바일 이용증", self) #버튼 객체 추가
+        self.monileApp.clicked.connect(self.monileApp_click)
+        
+        self.studyinfra = QPushButton("열람실", self) #버튼 객체 추가
+        self.studyinfra.clicked.connect(self.studyinfra_click)
+        
+        self.groupstudyRoom = QPushButton("그룹 스터디룸", self) #버튼 객체 추가
+        self.groupstudyRoom.clicked.connect(self.groupstudyRoom_click)
+        
+        self.mulitmedia_infra = QPushButton("멀티미디어 감상실", self) #버튼 객체 추가
+        self.mulitmedia_infra.clicked.connect(self.mulitmedia_infra_click)
+        
+        self.infoRounge_infra = QPushButton("정보검색라운지", self) #버튼 객체 추가
+        self.infoRounge_infra.clicked.connect(self.infoRounge_infra_click)
+        
+        self.selfReturn = QPushButton("자가대출반납기", self) #버튼 객체 추가
+        self.selfReturn.clicked.connect(self.selfReturn_click)
+        
+        self.copyPrint_infra = QPushButton("복사/출력실", self) #버튼 객체 추가
+        self.copyPrint_infra.clicked.connect(self.copyPrint_infra_click)
+        
+        self.start_btn = QPushButton("처음으로", self) #버튼 객체 추가
+        self.start_btn.clicked.connect(self.start_btn_click)
+        
+        #그리드 레이아웃에 버튼 위치 배치
+        infraMenuLayout.addWidget(self.monileApp, 0, 0)
+        infraMenuLayout.addWidget(self.studyinfra, 0, 1)
+        infraMenuLayout.addWidget(self.groupstudyRoom, 0, 2)
+        infraMenuLayout.addWidget(self.mulitmedia_infra, 0, 3)
+        infraMenuLayout.addWidget(self.infoRounge_infra, 1, 0)
+        infraMenuLayout.addWidget(self.selfReturn, 1, 1)
+        infraMenuLayout.addWidget(self.copyPrint_infra, 1, 2)
+        infraMenuLayout.addWidget(self.start_btn, 1, 3)
+        
+        
+        
+        self.widgetHeight+=90
+        if self.widgetHeight >= 650: #스크롤바 늘려주기
+            self.centerHeight+=90
+            self.lb_center.setFixedHeight(self.centerHeight)
+        centerLayout.addWidget(infraText)
+        centerLayout.setAlignment(Qt.AlignTop)
+        centerLayout.addLayout(infraMenuLayout)
+    
+    #  #  #  #  #  #  #  #  #  #  #  #
+    # 시설 안내 클릭 이벤트 버튼 #
+    #   #  #  #  #  #  #   #  #  #  #
+    
+    def monileApp_click(self): #모바일 이용증
+        print("monileApp_click button click")
+        self.infra_click()
+        
+    def studyinfra_click(self): #열람실
+        print("studyinfra_click button click")
+        self.infra_click()
+        
+    def groupstudyRoom_click(self): # 그룹 스터디룸
+        print("groupstudyRoom_click button click")
+        self.infra_click()
+        
+    def mulitmedia_infra_click(self): #멀티미디어 감상실
+        print("mulitmedia_infra_click button click")
+        self.infra_click()
+        
+    def infoRounge_infra_click(self): #정보검색라운지
+        print("infoRounge_infra_click button click")
+        self.infra_click()
+        
+    def selfReturn_click(self): #자가대출반납기
+        print("selfReturn_click button click")
+        self.infra_click()
+        
+    def copyPrint_infra_click(self): #복사 출력실
+        print("copyPrint_infra_click button click")
+        self.infra_click()
 
 
     #시작하자마자 뜨는 메시지
     def startMessage(self):
         print('start')
-        '''
-        self.startText1 = QLabel('안녕하세요')
-        self.startText1.setMaximumSize(150,30)
-        self.startText1.setFont(QFont('굴림',11))
-        self.startText1.setStyleSheet("color : black;" #라벨 꾸미기
-                              "border-style: solid;"
-                              "border-width: 1px;"
-                              "border-color: black;"
-                              "border-radius: 2px;"
-                              "background-color: #CEF6E3;"
-                            )
-        
-        self.startText2 = QLabel('원하시는 버튼을 클릭해주세요.\n채팅으로 검색하셔도 됩니다.')
-        self.startText2.setMaximumSize(260,50)
-        self.startText2.setFont(QFont('굴림',11))
-        self.startText2.setStyleSheet("color : black;" #라벨 꾸미기
-                              "border-style: solid;"
-                              "border-width: 1px;"
-                              "border-color: black;"
-                              "border-radius: 2px;"
-                              "background-color: #CEF6E3;"
-                            )
-        
-        #self.btnSetLabel = QLabel()  # 버튼 집합 라벨 설정
-        btnSetLayout = QHBoxLayout() # 버튼 집합 레이아웃 설정
-        
-        self.useTime_btn = QPushButton("이용 시간", self) #이용시간 버튼 객체 추가
-        self.useTime_btn.clicked.connect(self.useTime_click)
-        btnSetLayout.addWidget(self.useTime_btn)
-        
-        self.enter_btn = QPushButton("출입 안내", self) #찾아오는 길 버튼 객체 추가
-        self.enter_btn.clicked.connect(self.enter_click)
-        btnSetLayout.addWidget(self.enter_btn)
-        
-        self.infra_btn = QPushButton("시설 안내", self)
-        self.infra_btn.clicked.connect(self.infra_click)
-        btnSetLayout.addWidget(self.infra_btn)
-        
-        self.studyHelp_btn = QPushButton("연구학습지원", self) 
-        self.studyHelp_btn.clicked.connect(self.studyHelp_click)
-        btnSetLayout.addWidget(self.studyHelp_btn)
-        
-        self.findWay_btn = QPushButton("오시는 길", self) 
-        self.findWay_btn.clicked.connect(self.findWay_click)
-        btnSetLayout.addWidget(self.findWay_btn)
-        '''
-        """
-        self.centerLayout = QVBoxLayout()
-        start_set = starter()
-        print('12')
-        #self.centerLayout = QVBoxLayout() ###
-        self.centerLayout.addWidget(start_set.startText()[0])
-        self.centerLayout.addWidget(start_set.startText()[1])
-        
-        start_set.startMsg().setAlignment(Qt.AlignLeft)
-        self.centerLayout.addLayout(start_set.startMsg())  #레이아웃 추가  !!!!!!
-        
-        start_set.useTime_btn.clicked.connect(self.useTime_click)
-        start_set.enter_btn.clicked.connect(self.enter_click)
-        start_set.infra_btn.clicked.connect(self.infra_click)
-        # 버튼 event클래스 추가
-        """
         
         start_set = starter() #스타터 클래스 불러옴
         #중앙 라벨에 centerlayout을 레이아웃으로 설정
         self.lb_center.setLayout(start_set.start_def())
-        print('-=66-=')
+        
+        self.widgetHeight+=90
+        if self.widgetHeight >= 650: #스크롤바 늘려주기
+            self.centerHeight+=90
+            self.lb_center.setFixedHeight(self.centerHeight)
+        
         #메뉴 버튼에 클릭 이벤트 설정
         start_set.useTime_btn.clicked.connect(self.useTime_click)
         start_set.enter_btn.clicked.connect(self.enter_click)
@@ -419,7 +491,7 @@ if __name__ == '__main__':
     print("open app")
     #bookSearch('')
     centerLayout = QVBoxLayout()
-    
+    #centerHeight = 650
     app = QApplication(sys.argv)
     ex = MyApp()
     start = ex.startMessage()
